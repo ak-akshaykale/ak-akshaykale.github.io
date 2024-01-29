c4242244179b5948eaf3bca2cccab1f283a37e02
snehal65 <sonawanesnehal444@gmail.com>
2024-01-29 18:24:17 +0530

    demo app

---
+++ res/demo/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hosting @kbrand!!
+        </p>
+        <a
+          className="App-link"
+          href="https://ak-akshaykale.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          more
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
